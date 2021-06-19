21ded90e03dd884c3bec49a76a1e24755a7f4a30
JayDee <javedkhan889971@gmail.com>
2021-06-20 00:48:25 +0500

    Add article create & update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,32 @@ class ArticlesController < ApplicationController
     @article = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article edited successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
