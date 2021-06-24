abe549cf1b393908f3d2636fd1794ce04f957eb3
JayDee <javedkhan889971@gmail.com>
2021-06-24 15:55:09 +0500

    Added Bootsraps Styling & Updated Views

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,10 @@
 const {environment} = require('@rails/webpacker')
 
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
 
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
 
 module.exports = environment
