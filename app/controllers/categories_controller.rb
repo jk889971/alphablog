70f2e3cb2888aec0ec286c1e076e6b042ec00988
JayDee <javedkhan889971@gmail.com>
2021-08-14 18:44:35 +0500

    Categories views created and restricted actions

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,51 @@
+class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+
+  def new
+
+    @category = Category.new
+
+  end
+
+  def create
+
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+
+  end
+
+  def index
+
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+
+  end
+
+  def show
+
+    @category = Category.find(params[:id])
+
+  end
+
+  private
+
+  def category_params
+
+    params.require(:category).permit(:name)
+
+  end
+
+  def require_admin
+
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "You're not allowed to perform this action."
+      redirect_to categories_path
+    end
+
+  end
+
+end
